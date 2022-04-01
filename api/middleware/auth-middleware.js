70ee0e23152cf245c720edb142bd6b84c75b26f4
Your Name <mari@Maribels-MacBook-Pro.local>
2022-04-01 13:40:17 -0500

    users model and auth-middlware figured

---
+++ api/middleware/auth-middleware.js
@@ -0,0 +1,26 @@
+const Users = require('../users/users-model');
+
+const validateUsername = async (req, res, next) => {
+    try {
+        const users = await Users.findBy({ username: req.body.username })
+        if(!users.length) {
+            next()
+        } else next({  status: 401, message: 'username taken' })
+    } catch (error) {
+        next(error)
+    }
+};
+
+const validateData = (req, res, next) => {
+    const {username, password} = req.body;
+    if(!username || !password) {
+        res.status(400).json({ message: 'username and password required' })
+    } else {
+        next()
+    }
+};
+
+module.exports = {
+    validateUsername,
+    validateData
+}
